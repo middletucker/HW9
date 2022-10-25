53f22c8488a2ee78d2e2b15b8876873a7b97dcd9
middletucker <80505827+middletucker@users.noreply.github.com>
2022-10-25 19:55:23 +0000

    correct grade and count P3

--- P3/main3.cpp
+++ P3/main3.cpp
@@ -6,22 +6,28 @@ using namespace std;
 int TrueFalse(char matrix[][5], int R, int C);
 
 int main() {
+  double grades[6];
   double grade1, grade2, grade3, grade4, grade5, grade6;
   char tests[6][5] = {
       {'T', 'F', 'T', 'T', 'T'}, {'T', 'T', 'T', 'T', 'T'},
       {'T', 'T', 'F', 'F', 'T'}, {'F', 'T', 'F', 'F', 'F'},
       {'F', 'F', 'F', 'F', 'F'}, {'T', 'T', 'F', 'T', 'F'},
   };
-  grade1 = TrueFalse(tests, 1, 5);
+  grades[0] = TrueFalse(tests, 0, 4);
+  grades[1] = TrueFalse(tests, 1, 4);
+  grades[2] = TrueFalse(tests, 2, 4);
+ 
 }
 int TrueFalse(char matrix[][5], int R, int C) {
   double count = 0, grade = 0;
-  for (int i = 0; i == R; i++) {
-    for (int j = 1; j < C; j++) {
-      if (matrix[i][j] == 'T') {
-        count = count + 5;
-      }
+  // for (int i = 0; i == R; i++) {
+  for (int j = 0; j < C; j++) {
+    if (matrix[R][j] == 'T') {
+      count = count + 5;
     }
-    grade = count / C;
-    return grade;
   }
+  // }
+  cout << count << endl;
+  grade = (count / (C * 5)) * 100;
+  return grade;
+}
